5b1f4627994675a0d5bcdd10d8591524cf0827b6
Muneeb Hashmi <50544578+muneebhashmi7712@users.noreply.github.com>
2022-05-01 20:41:31 +0500

    Add files via upload

---
+++ FastAPI - Deploying ML Models/app.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:40:41 2020
+
+@author: win10
+"""
+
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from BankNotes import BankNote
+import numpy as np
+import pickle
+import pandas as pd
+# 2. Create the app object
+app = FastAPI()
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+@app.post('/predict')
+def predict_banknote(data:BankNote):
+    data = data.dict()
+    variance=data['variance']
+    skewness=data['skewness']
+    curtosis=data['curtosis']
+    entropy=data['entropy']
+   # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    if(prediction[0]>0.5):
+        prediction="Fake note"
+    else:
+        prediction="Its a Bank note"
+    return {
+        'prediction': prediction
+    }
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
